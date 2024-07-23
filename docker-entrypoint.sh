ebee9500e3e2d5a7c32eb0c3316892f749462e5f
GenericMale <genericmale00@gmail.com>
2024-07-23 23:17:40 +0200

    Add IP Check & filter duplicate IPs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-: "${VPN_SERVER_COUNT:=1}" #How many of the fastest servers to rotate
+: "${VPN_SERVER_COUNT:=1}"  #How many of the fastest servers to rotate
 : "${VPN_SERVER_FILTER:=.}" #Additional jq filter to apply to server list
 
 : "${OPENVPN_USER_PASS_FILE:=/etc/openvpn/protonvpn.auth}"
 : "${OPENVPN_CONFIG_FILE:=/etc/openvpn/protonvpn.ovpn}"
 
 : "${PROTON_API_URL:=https://api.protonvpn.ch/vpn/logicals}"
+: "${IP_URL:=https://ifconfig.co/json}"
 : "${PROTON_TIER:=2}" #Proton Tier. 0=Free, 1=Basic, 2=Plus, 3=Visionary
 
 if [ ! -f $OPENVPN_USER_PASS_FILE ]; then
-    echo $OPENVPN_USER > $OPENVPN_USER_PASS_FILE
-    echo $OPENVPN_PASS >> $OPENVPN_USER_PASS_FILE
+    echo $OPENVPN_USER >$OPENVPN_USER_PASS_FILE
+    echo $OPENVPN_PASS >>$OPENVPN_USER_PASS_FILE
 fi
 
-while true
-do
-
-    if pgrep -x openvpn > /dev/null; then
+while true; do
+    if pgrep -x openvpn >/dev/null; then
         #Call API without VPN to get proper scores
         echo "Disconnecting..."
 
@@ -26,15 +25,14 @@ do
         iptables -P INPUT ACCEPT
 
         pkill openvpn
-        while pgrep -x openvpn > /dev/null; do sleep 1; done
+        while pgrep -x openvpn >/dev/null; do sleep 1; done
     fi
 
     echo "Fetching Server List..."
-    servers=$(wget -q -O- $PROTON_API_URL | jq ".LogicalServers | map(select(.Tier <= $PROTON_TIER and .Status == 1)) | sort_by(.Score) | $VPN_SERVER_FILTER | map(.Servers[0].EntryIP)[:$VPN_SERVER_COUNT][]" | tr -d '"')
+    get_servers=".LogicalServers | map(select(.Tier <= $PROTON_TIER and .Status == 1)) | sort_by(.Score)"
+    get_unique_ip_list="map({(.Servers[].EntryIP):1}) | add | keys_unsorted | .[:$VPN_SERVER_COUNT][]"
+    servers=$(wget -q -O- $PROTON_API_URL | jq -r "$get_servers | $VPN_SERVER_FILTER | $get_unique_ip_list")
     echo ${servers//$'\n'/ }
-
-    echo "Connecting..."
-    ovpn_cmd="openvpn --config $OPENVPN_CONFIG_FILE --auth-user-pass $OPENVPN_USER_PASS_FILE --remote ${servers//$'\n'/' --remote '}"
 
     #Engage Kill Switch
     iptables -F
@@ -50,23 +48,32 @@ do
     iptables -A INPUT -s 172.16.0.0/12 -i eth0 -j ACCEPT
     iptables -A OUTPUT -d 172.16.0.0/12 -o eth0 -j ACCEPT
 
+    #Start in background
+    echo "Connecting..."
+    eval "openvpn --config $OPENVPN_CONFIG_FILE --auth-user-pass $OPENVPN_USER_PASS_FILE --remote ${servers//$'\n'/' --remote '} &"
+
+    if [ "$IP_URL" ]; then
+        #Wait for new IP. Kill Switch blocks until VPN is up.
+        while true; do
+            ip=$(wget -T 3 -q -O- $IP_URL 2>/dev/null)
+            if [[ "$?" -eq 0 ]]; then break; fi
+            sleep 1
+        done
+        echo $ip | jq -r '"New IP: \(.ip) - \(.country), \(.asn_org)"'
+    fi
+
     if [ -z $VPN_RECONNECT ]; then
-        # Start OpenVPN in foreground if we don't need to reconnect
-        eval "$ovpn_cmd"
+        #Halt until OpenVPN is interrupted if reconnect not set
+        wait
+    elif [[ $VPN_RECONNECT == *":"* ]]; then
+        #Convert HH:MM to seconds from now
+        sleep=$(($(date -d $VPN_RECONNECT +%s) - $(date +%s)))
+        if [ $sleep -lt 0 ]; then sleep=$((86400 + sleep)); fi
+
+        echo "Reconnecting in ${sleep}s ($VPN_RECONNECT)."
+        sleep $sleep
     else
-        # Start in background and sleep
-        eval "$ovpn_cmd &"
-
-        if [[ $VPN_RECONNECT == *":"* ]]; then
-            # Convert HH:MM to seconds from now
-            sleep=$(($(date -d $VPN_RECONNECT +%s) - $(date +%s)))
-            if [ $sleep -lt 0 ]; then sleep=$((86400 + sleep)); fi
-
-            echo "Reconnecting in ${sleep}s ($VPN_RECONNECT)."
-            sleep $sleep
-        else
-            echo "Reconnecting in $VPN_RECONNECT."
-            sleep $VPN_RECONNECT
-        fi
+        echo "Reconnecting in $VPN_RECONNECT."
+        sleep $VPN_RECONNECT
     fi
 done
