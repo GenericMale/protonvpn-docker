f713f41b57a638a15421636f9cecb6a7becd1a33
GenericMale <genericmale00@gmail.com>
2024-08-02 02:01:48 +0200

    add testing workflow

--- src/healthcheck.sh
+++ src/healthcheck.sh
@@ -2,7 +2,7 @@
 
 #check if tun device has ip
 tun_ip=$(ip -4 addr show tun0 | grep inet | xargs echo -n | cut -d' ' -f2 2>/dev/null)
-if [[ $tun_ip =~ ^\\d+\.\\d+\.\\d+\.\\d+\/\\d+$ ]]; then
+if [[ $tun_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/[0-9]+$ ]]; then
   exit 0
 else
   exit 1
