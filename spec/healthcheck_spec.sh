eb3271c3339ec4369c776fa2e1932ab3c76369bb
GenericMale <genericmale00@gmail.com>
2024-08-02 02:28:12 +0200

    run tests on busybox and on bash for coverage

--- spec/healthcheck_spec.sh
+++ spec/healthcheck_spec.sh
@@ -1,18 +1,18 @@
 Describe "healthcheck.sh"
   It "fails when tun device doesn't exist"
-    Mock ip
+    ip() {
       echo ""
-    End
+    }
     When run source src/healthcheck.sh
     The status should be failure
   End
 
   It "succeeds when tun device has ip"
-    Mock ip
+    ip() {
       echo "2: tun0: <POINTOPOINT,MULTICAST,NOARP,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UNKNOWN group default qlen 1000
                 inet 10.96.0.10/16 scope global tun0
                    valid_lft forever preferred_lft forever"
-    End
+    }
     When run source src/healthcheck.sh
     The status should be success
   End
--- src/healthcheck.sh
+++ src/healthcheck.sh
@@ -2,7 +2,7 @@
 
 #check if tun device has ip
 tun_ip=$(ip -4 addr show tun0 | grep inet | xargs echo -n | cut -d' ' -f2 2>/dev/null)
-if [[ $tun_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/[0-9]+$ ]]; then
+if echo "$tun_ip" | grep -qE "^([0-9]+\.){3}[0-9]+/[0-9]+$"; then
   exit 0
 else
   exit 1
